f383ab407b21e29ca49852ac23f55fd9bf7ea276
Divyansh2903 <singhdivyansh29@yahoo.com>
2025-09-10 03:21:32 +0530

    Server fixes

--- be/server.js
+++ be/server.js
@@ -1,12 +1,10 @@
-
 const app = require('./app');
 
-if (process.env.NODE_ENV !== 'production' || process.env.NODE_ENV ==undefined) {
+if (process.env.NODE_ENV !== 'test') {  
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+    console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`);
   });
 }
-
 
 module.exports = app;
